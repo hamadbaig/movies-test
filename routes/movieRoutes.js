cefc20c994b4225e42bf2641ffb23e6e7654602c
hamadbaig <hammad.ahmed.connect@gmail.com>
2024-11-28 16:35:49 +0500

    changes no

--- routes/movieRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getMovies } = require("../controllers/movieController");
-const router = express.Router();
-
-router.get("/movies", getMovies);
-
-module.exports = router;
